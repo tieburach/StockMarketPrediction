52a33afff6bc51497810c8d517bf9a4608e1879a
Tieburach <witalis.kinasz@gmail.com>
2020-09-09 19:23:00 +0200

    Initial commit

---
+++ src/main/java/com/tieburach/stockprediction/StockMarketPredictionApplication.java
@@ -0,0 +1,13 @@
+package com.tieburach.stockprediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketPredictionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockMarketPredictionApplication.class, args);
+    }
+
+}
